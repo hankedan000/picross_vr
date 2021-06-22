75f91c300fc2789534e183a1bf2ee20b92ca7abd
hankedan000 <dany32412@gmail.com>
2021-06-22 01:02:56 -0400

    Adds ability to highlight BaseCube

--- scenes/BaseCube/BaseCube.gd
+++ scenes/BaseCube/BaseCube.gd
@@ -11,3 +11,14 @@ func set_unsolved():
 		var mat : Material = mesh_inst.get_surface_material(0)
 		if mat is SpatialMaterial:
 			mat.albedo_texture = UnsolvedImage
+
+func clear_highlight_color():
+	set_highlight_color(Color.white)
+
+func set_highlight_color(color : Color):
+	var light_color = color
+	light_color.s = 0.2
+	for mesh_inst in $meshes.get_children():
+		var mat : Material = mesh_inst.get_surface_material(0)
+		if mat is SpatialMaterial:
+			mat.albedo_color = light_color
--- scenes/BaseCube/BaseCubePreview.gd
+++ scenes/BaseCube/BaseCubePreview.gd
@@ -1,9 +1,17 @@
 extends Spatial
 
+var cubes = []
+
 func _ready():
-	$BaseCube.set_unsolved()
+	$GreenCube.set_unsolved()
+	$GreenCube.set_highlight_color(Color.green)
+	cubes.append($GreenCube)
+	$RedCube.set_unsolved()
+	$RedCube.set_highlight_color(Color.red)
+	cubes.append($RedCube)
 
 func _process(delta):
-	$BaseCube.transform = $BaseCube.transform.rotated(Vector3(1,0,0), 2 * PI / 16 * delta)
-	$BaseCube.transform = $BaseCube.transform.rotated(Vector3(0,1,0), 2 * PI / 32 * delta)
-	$BaseCube.transform.orthonormalized()
+	for cube in cubes:
+		cube.transform = cube.transform.rotated(Vector3(1,0,0), 2 * PI / 16 * delta)
+		cube.transform = cube.transform.rotated(Vector3(0,1,0), 2 * PI / 32 * delta)
+		cube.transform.orthonormalized()
