eb9b0dff4e7f05a5b2b806273cdeb6f9067e042f
hankedan000 <dany32412@gmail.com>
2021-06-27 18:18:19 -0400

    Adds platypus to test

--- scripts/test/PicrossPuzzleTest.gd
+++ scripts/test/PicrossPuzzleTest.gd
@@ -46,11 +46,233 @@ const HORSE_PUZZLE = {
 	]
 }
 
+const PLATYPUS = {
+	"dims": [12, 3, 5],
+	"hints": [
+		[
+			[{
+				"num": 2,
+				"type": 1
+			}, null, null, null, {
+				"num": 2,
+				"type": 1
+			}],
+			[{
+				"num": 8,
+				"type": 0
+			}, null, null, null, {
+				"num": 8,
+				"type": 0
+			}],
+			[{
+				"num": 6,
+				"type": 0
+			}, {
+				"num": 9,
+				"type": 0
+			}, {
+				"num": 9,
+				"type": 0
+			}, {
+				"num": 9,
+				"type": 0
+			}, {
+				"num": 6,
+				"type": 0
+			}]
+		],
+		[
+			[null, {
+				"num": 0,
+				"type": 0
+			}, {
+				"num": 1,
+				"type": 0
+			}, {
+				"num": 0,
+				"type": 0
+			}, null],
+			[{
+				"num": 2,
+				"type": 0
+			}, {
+				"num": 1,
+				"type": 0
+			}, {
+				"num": 1,
+				"type": 0
+			}, {
+				"num": 1,
+				"type": 0
+			}, {
+				"num": 2,
+				"type": 0
+			}],
+			[null, {
+				"num": 2,
+				"type": 0
+			}, {
+				"num": 2,
+				"type": 0
+			}, {
+				"num": 2,
+				"type": 0
+			}, null],
+			[null, {
+				"num": 2,
+				"type": 0
+			}, {
+				"num": 2,
+				"type": 0
+			}, {
+				"num": 2,
+				"type": 0
+			}, null],
+			[null, {
+				"num": 2,
+				"type": 0
+			}, {
+				"num": 2,
+				"type": 0
+			}, {
+				"num": 2,
+				"type": 0
+			}, null],
+			[null, {
+				"num": 2,
+				"type": 0
+			}, {
+				"num": 2,
+				"type": 0
+			}, {
+				"num": 2,
+				"type": 0
+			}, null],
+			[null, {
+				"num": 2,
+				"type": 0
+			}, {
+				"num": 2,
+				"type": 0
+			}, {
+				"num": 2,
+				"type": 0
+			}, null],
+			[{
+				"num": 2,
+				"type": 0
+			}, {
+				"num": 2,
+				"type": 0
+			}, {
+				"num": 2,
+				"type": 0
+			}, {
+				"num": 2,
+				"type": 0
+			}, {
+				"num": 2,
+				"type": 0
+			}],
+			[{
+				"num": 2,
+				"type": 0
+			}, {
+				"num": 2,
+				"type": 0
+			}, {
+				"num": 2,
+				"type": 0
+			}, {
+				"num": 2,
+				"type": 0
+			}, {
+				"num": 2,
+				"type": 0
+			}],
+			[null, {
+				"num": 2,
+				"type": 0
+			}, {
+				"num": 2,
+				"type": 0
+			}, {
+				"num": 2,
+				"type": 0
+			}, null],
+			[{
+				"num": 0,
+				"type": 0
+			}, {
+				"num": 2,
+				"type": 0
+			}, {
+				"num": 2,
+				"type": 0
+			}, {
+				"num": 2,
+				"type": 0
+			}, {
+				"num": 0,
+				"type": 0
+			}],
+			[{
+				"num": 0,
+				"type": 0
+			}, {
+				"num": 1,
+				"type": 0
+			}, {
+				"num": 1,
+				"type": 0
+			}, {
+				"num": 1,
+				"type": 0
+			}, {
+				"num": 0,
+				"type": 0
+			}]
+		],
+		[
+			[null, {
+				"num": 1,
+				"type": 0
+			}, null],
+			[{
+				"num": 2,
+				"type": 1
+			}, null, null],
+			[null, null, null],
+			[null, null, null],
+			[null, null, null],
+			[null, null, null],
+			[null, null, null],
+			[null, null, null],
+			[{
+				"num": 2,
+				"type": 1
+			}, null, null],
+			[null, null, null],
+			[null, null, {
+				"num": 3,
+				"type": 0
+			}],
+			[null, {
+				"num": 3,
+				"type": 0
+			}, null]
+		]
+	],
+	"name": "Platypus"
+}
+
 func _ready():
-	var puzzle := PicrossPuzzleUtils.fromJSON(HORSE_PUZZLE)
-	var shape := PicrossSolver.bruteForceSolve(puzzle)
-	if shape != null:
-		print("%s is solvable!" % [puzzle.name()])
-		print(shape.cells())
-	else:
-		print("%s is not solvable :(" % [puzzle.name()])
+	var puzzles_json = [HORSE_PUZZLE,PLATYPUS]
+	for puzzle_json in puzzles_json:
+		var puzzle := PicrossPuzzleUtils.fromJSON(puzzle_json)
+		var shape := PicrossSolver.bruteForceSolve(puzzle)
+		if shape != null:
+			print("%s is solvable!" % [puzzle.name()])
+			print(shape.cells())
+		else:
+			print("%s is not solvable :(" % [puzzle.name()])
